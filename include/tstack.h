3a4820513eeec276b74b3452003c243947465723
egorisan <epodcherednichenko@gmail.com>
2025-05-18 22:36:47 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -3,8 +3,41 @@
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
-class TStack {
-  // добавьте код стека
+private:
+    T data[size];
+    int topIndex;
+
+public:
+    TStack() : topIndex(-1) {}
+
+    bool IsEmpty() const {
+        return topIndex == -1;
+    }
+
+    bool IsFull() const {
+        return topIndex == size - 1;
+    }
+
+    void Push(const T& value) {
+        if (IsFull()) {
+            throw std::overflow_error("Stack overflow");
+        }
+        data[++topIndex] = value;
+    }
+
+    T Pop() {
+        if (IsEmpty()) {
+            throw std::underflow_error("Stack underflow");
+        }
+        return data[topIndex--];
+    }
+
+    T Top() const {
+        if (IsEmpty()) {
+            throw std::underflow_error("Stack is empty");
+        }
+        return data[topIndex];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
